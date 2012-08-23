19e2774e3097038a11d6b1ca85e0fbd15c567f2f
ggear <graham.gear@gmail.com>
2012-08-23 10:50:15 +0100

    update list script

--- src/main/assembly/layout/opt/cluster/list.sh
+++ src/main/assembly/layout/opt/cluster/list.sh
@@ -1,34 +1,75 @@
 #!/bin/sh
 
-function line_item() {
-  echo "" && echo "----" && echo "" 
+function line_item_header() {
+  echo "-------------------------------------------------------------------------------" && echo "$1" && echo "-------------------------------------------------------------------------------" && echo "" 
+}
+
+function line_item_ip() {
+  cat $TMP_FILE | grep $1 | cut -d: -f2 | awk '{print $3}'
+}
+
+function line_item_host() {
+   line_item_ip $1 | xargs -n 1 -I £ dig +short -x £ | sed 's/\(.*\)./\1/'
+}
+
+function line_item_url() {
+  line_item_host $1 | xargs -n 1 -I £ echo "http://"£":"$2"/"$3
+}
+
+function line_item_ssh() {
+  line_item_host $1 | xargs -n 1 -I £ echo "ssh -o StrictHostKeyChecking=no whirr@"£
 }
 
 TMP_DIR_TEMPLATE="/tmp/`echo ${0%/} | awk -F/ '{print \$NF}'`.XXXXXXXXXXXXXX"
 TMP_DIR=`mktemp -d $TMP_DIR_TEMPLATE`
 TMP_FILE="$TMP_DIR/whirr_list-cluster.txt"
 
-line_item
-echo -n "Querying cluster ... "
+echo ""
+line_item_header ""
+echo ""
+
+line_item_header "Whirr Cluster"
 whirr list-cluster | grep -v "Running " > $TMP_FILE
-echo "done"
+cat $TMP_FILE
+echo ""
 
-line_item
-echo "Whirr Cluster:" && echo ""
-cat $TMP_FILE | grep -v "Running "
+line_item_header "CM Servers"
+line_item_url "cmserver" "7180"
+echo ""
+line_item_ssh "cmserver"
+echo ""
 
-line_item
-echo "CM Server IP:" && echo ""
-cat $TMP_FILE | grep "cmserver" | cut -d: -f2 | awk '{print $3}'
+line_item_header "CM Nodes"
+line_item_host "cmnode"
+echo ""
+line_item_ssh "cmnode"
+echo ""
 
-line_item
-echo "CM Server URL:" && echo ""
-echo 'http://'`dig +short -x $(cat  $TMP_FILE | grep cmserver| cut -d: -f2 | awk '{print $3}') | head -c -2`':7180'
+line_item_header "CDH Name Nodes"
+line_item_url "hadoop-namenode" "50070"
+echo ""
+line_item_ssh "hadoop-namenode"
+echo ""
 
-line_item
-echo "CDH Node IPs:" && echo ""
-cat $TMP_FILE | grep "cmnode" | cut -d: -f2 | awk '{print $3}'
+line_item_header "CDH Job Trackers"
+line_item_url "hadoop-jobtracker" "50030"
+echo ""
+line_item_ssh "hadoop-jobtracker"
+echo ""
 
-line_item
+line_item_header "CDH Data Nodse"
+line_item_url "hadoop-datanode" "50010"
+echo ""
+line_item_ssh "hadoop-datanode"
+echo ""
+
+line_item_header "CDH Task Trackers"
+line_item_url "hadoop-tasktracker" "50060"
+echo ""
+line_item_ssh "hadoop-tasktracker"
+echo ""
+
+line_item_header ""
+echo ""
 
 rm -rf $TMP_DIR
