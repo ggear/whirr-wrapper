0f45fcc1ca6d1e65fe2442e65ecb4b47f4efa692
ggear <graham.gear@gmail.com>
2012-08-23 11:17:11 +0100

    update list script

--- src/main/assembly/layout/opt/cluster/list.sh
+++ src/main/assembly/layout/opt/cluster/list.sh
@@ -4,12 +4,16 @@ function line_item_header() {
   echo "-------------------------------------------------------------------------------" && echo "$1" && echo "-------------------------------------------------------------------------------" && echo "" 
 }
 
+function line_item_line() {
+  cat $TMP_FILE | grep $1
+}
+
 function line_item_ip() {
-  cat $TMP_FILE | grep $1 | cut -d: -f2 | awk '{print $3}'
+   line_item_line $1 | cut -d: -f2 | awk '{print $3}'
 }
 
 function line_item_host() {
-   line_item_ip $1 | xargs -n 1 -I £ dig +short -x £ | sed 's/\(.*\)./\1/'
+  line_item_ip $1 | xargs -n 1 -I £ dig +short -x £ | sed 's/\(.*\)./\1/'
 }
 
 function line_item_url() {
@@ -19,6 +23,27 @@ function line_item_url() {
 function line_item_ssh() {
   line_item_host $1 | xargs -n 1 -I £ echo "ssh -o StrictHostKeyChecking=no whirr@"£
 }
+
+function line_items_host() {
+	if [ $(line_item_line "$2" | wc -l) -gt 0 ]; then 
+	  line_item_header "$1"
+	  line_item_host "$2"
+	  echo ""
+	  line_item_ssh "$2"
+	  echo ""
+	fi
+}
+
+function line_items_url() {
+  if [ $(line_item_line "$2" | wc -l) -gt 0 ]; then 
+    line_item_header "$1"
+    line_item_url "$2" "$3" "$4"
+    echo ""
+    line_item_ssh "$2"
+    echo ""
+  fi
+}
+
 
 TMP_DIR_TEMPLATE="/tmp/`echo ${0%/} | awk -F/ '{print \$NF}'`.XXXXXXXXXXXXXX"
 TMP_DIR=`mktemp -d $TMP_DIR_TEMPLATE`
@@ -33,43 +58,15 @@ whirr list-cluster | grep -v "Running " > $TMP_FILE
 cat $TMP_FILE
 echo ""
 
-line_item_header "CM Servers"
-line_item_url "cmserver" "7180"
-echo ""
-line_item_ssh "cmserver"
-echo ""
-
-line_item_header "CM Nodes"
-line_item_host "cmnode"
-echo ""
-line_item_ssh "cmnode"
-echo ""
-
-line_item_header "CDH Name Nodes"
-line_item_url "hadoop-namenode" "50070"
-echo ""
-line_item_ssh "hadoop-namenode"
-echo ""
-
-line_item_header "CDH Job Trackers"
-line_item_url "hadoop-jobtracker" "50030"
-echo ""
-line_item_ssh "hadoop-jobtracker"
-echo ""
-
-line_item_header "CDH Data Nodse"
-line_item_url "hadoop-datanode" "50010"
-echo ""
-line_item_ssh "hadoop-datanode"
-echo ""
-
-line_item_header "CDH Task Trackers"
-line_item_url "hadoop-tasktracker" "50060"
-echo ""
-line_item_ssh "hadoop-tasktracker"
-echo ""
+line_items_url "CM Servers" "cmserver" "7180" 
+line_items_host "CM Nodes" "cmnode"
+line_items_url "CDH Name Nodes" "hadoop-namenode" "50070" 
+line_items_url "CDH Job Trackers Nodes" "hadoop-jobtracker" "50030" 
+line_items_url "CDH Data Nodes" "hadoop-datanode" "50010" 
+line_items_url "CDH Task Tracker Nodes" "hadoop-tasktracker" "50060" 
 
 line_item_header ""
 echo ""
 
 rm -rf $TMP_DIR
+
